251a1706ed3caa9c69d628bcd31504a44c547c7b
jdRanda <randa.jtd@gmail.com>
2022-11-20 11:15:47 +0000

    expanding memory write conditions

--- src/memoryBus.cpp
+++ src/memoryBus.cpp
@@ -13,13 +13,32 @@ byte_t MemoryBus::readByte(word_t address) const
     return memory[address];
 }
 
+//TODO: magic numbers
 void MemoryBus::writeByte(word_t address, byte_t value)
 {
     if (0 < address || address >= c_MEMORY_SIZE)
     { 
         throw std::runtime_error("Trying to write to invalid memory address!");
     }
+    
+    if (0xFEA0u <= address || address <= 0xFEFFu)
+    {
+        throw std::runtime_error("Trying to write to non useable memory address!");
+    }
 
+    //echo from echo ram
+    if (0xE000u <= address || address <= 0xFDFFu)
+    {
+        throw std::runtime_error("Trying to write to Echo memory address!");
+        // word_t echo_address{ address - (0xDFFFu - 0xC000u) };
+        // memory[echo_address] = value;
+    }
+    //echo into echo ram
+    if (0xC000u <= address || address <= 0xDFFFu)
+    {
+        word_t echo_address{ address + (0xDFFFu - 0xC000u) };
+        memory[echo_address] = value;
+    }
 
 
     memory[address] = value; 
