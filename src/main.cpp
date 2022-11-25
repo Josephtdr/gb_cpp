0d9720c85d92767338e0bfdf294f430866b1015f
jdRanda <randa.jtd@gmail.com>
2022-11-25 10:28:27 +0000

    comment

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "unistd.h"
 #include "cpu.h"
 
-
+//https://www.learncpp.com/cpp-tutorial/timing-your-code/
 class Timer
 {
 private:
@@ -13,21 +13,17 @@ private:
     using Second = std::chrono::duration<double, std::ratio<1> >;
 
     std::chrono::time_point<Clock> m_beg{ Clock::now() };
-
-    double frameRate{ 0.016666666666666 }; //in seconds
+    double frameRate{ 1.0 / 60.0 }; //in seconds
 
 public:
-
     void reset()
     {
         m_beg = Clock::now();
     }
-
     double elapsed() const
     {
         return std::chrono::duration_cast<Second>(Clock::now() - m_beg).count();
     }
-
     double nextFrameIn() const
     {
         return frameRate - elapsed();
